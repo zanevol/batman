6b7635c8c460325716f6ddc2bd15ffbbdcd6b6c6
zanevol <62759213+zanevol@users.noreply.github.com>
2020-03-27 22:37:23 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+	let tabsItem = $('.tabs-item');
+
+
+	tabsItem.on('click', function (event) {
+		event.preventDefault();
+		let activeContent = $(this).attr('href');
+		$('.visible').toggleClass('visible');
+		$(activeContent).toggleClass('visible');
+		$(tabsItem).toggleClass('tabs-item-active');
+		$('.tabs-item-active').toggleClass('tabs-item-active');
+		$(this).toggleClass('tabs-item-active');
+	});
+
+
+});
